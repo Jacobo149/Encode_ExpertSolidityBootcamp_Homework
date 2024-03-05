98c7ab2a31f1bf8d9d073f9f34ec36bc61f29933
Jacobo149 <jtp323@lehigh.edu>
2024-03-05 12:46:31 -0500

    Homework 4 Complete

---
+++ Homework_3/hw3.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.8.17;
+
+contract hw3 {
+    uint256 s;
+
+    function memvar(uint256 a) external {
+        uint256[1] memory b;
+        b = [a];
+        s = b[0];
+    }
+    
+}
